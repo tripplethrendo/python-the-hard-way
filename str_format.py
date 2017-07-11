fe68f0895cc76bdf7b8950176f2e8285eab3bdd1
tripplethrendo <tripplethrendo@gmail.com>
2017-07-11 10:45:31 -0700

    Learning string formatting!

---
+++ str_format.py
@@ -0,0 +1,7 @@
+age = 9000
+name = 'tendo'
+origin = 'digital'
+
+print('{0} was {1} years old when he wrote this {2} script'.format(name, age, origin))
+print('Why is {0} playing with that {1} reader?'.format(name, origin))
+print('I am pretty sure about my {0} communication skills.'.format(origin))
